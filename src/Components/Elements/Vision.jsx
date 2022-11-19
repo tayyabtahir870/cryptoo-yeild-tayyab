d7895da88311dc4e5793e934406947c1ae0c97de
tayyabtahir870@gmail.com <tayyabtahir870@gmail.com>
2022-11-19 23:50:30 +0500

    yeild

--- src/Components/Elements/Vision.jsx
+++ src/Components/Elements/Vision.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
 
 function Vision() {
   return (
-    <div>
-        
+    <div className="card1">
+      <div className="container text-center py-5">
+        <div>
+          <p className="card-text1 mt-5">OUR VISION</p>
+          <p className="card-text2">Cryptocurrency in <br />
+Every Wallet™</p>
+         <div >
+            <div>
+                <p>vsdv</p>
+                <p>vsdvsd</p>
+            </div>
+            <div></div>
+            <div>
+                <p>sdvsd</p>
+                <p>sdvsd</p>
+            </div>
+         </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Vision
+export default Vision;
